5328ff392219c6236dd2f76616e6f1cd0646a49e
tuquepantin <tuque250@gmail.com>
2023-06-17 00:52:50 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -5,9 +5,8 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
+    name = db.Column(db.String(20), nullable=False)
+    
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -17,3 +16,38 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+    population = db.Column(db.String(25), nullable=False)
+    diameter = db.Column(db.String(25), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "diameter": self.diameter,
+        }
+
+class Character (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+    gender = db.Column(db.String(25), nullable=False)
+    height = db.Column(db.String(25), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "height": self.height,
+        }
+
+class Favorite (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_id = db.Column(db.Integer, db.ForeignKey("character.id"))
+    planet_id = db.Column(db.Integer, db.ForeignKey("planet.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    
